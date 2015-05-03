a1e6c999fffc8442ab704a5aaab69655ca8564fd
nitsanp <pnitsan@gmail.com>
2015-05-03 16:29:45 +0300

    updating main page structure

--- smartfrig/app/App2/App.xaml.cs
+++ smartfrig/app/App2/App.xaml.cs
@@ -35,6 +35,16 @@ namespace App2
         {
             this.InitializeComponent();
             this.Suspending += this.OnSuspending;
+
+
+            //
+            SqlConnection myConnection = new SqlConnection("user id=username;" +
+                                       "password=password;server=serverurl;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=database; " +
+                                       "connection timeout=30");
+
+            //
         }
 
         /// <summary>
